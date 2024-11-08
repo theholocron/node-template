869c1b737ea736de9b3dfd9eaf9c183eee49c5a8
Newton <5769156+iamnewton@users.noreply.github.com>
2024-11-08 14:15:20 -0800

    fix: add in tests
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest";
-import { add } from "./index";
+import add from "./index";
 
 describe("add function", () => {
 	test("should return the correct sum", () => {

982690715f1e90b048bde623e74bdf1c24416209
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-03 19:01:12 -0700

    feat: ✨ publish node-template-site package for main docs site (#140)
    * feat: ✨ publish node-template-site package for main docs site
    
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>
    
    * chore: 🔧 use holocron npm bump-versions in release prepareCmd
    
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>
    
    * chore: 🔧 sort release config options lexicographically
    
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- release.config.ts
+++ release.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "@theholocron/semantic-release-config";
 
 export default defineConfig({
+	assets: ["CHANGELOG.md", "docs/package.json", "package.json"],
 	branches: ["main", { name: "alpha", prerelease: true }],
+	exec: {
+		prepareCmd: "pnpm exec holocron npm bump-versions ${nextRelease.version}",
+		publishCmd: "pnpm --filter @theholocron/node-template-site publish --access public --no-git-checks",
+	},
 	npm: { access: "public" },
 });

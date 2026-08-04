5f0ebd7d42613f01b563c273db478998da1a7e1f
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-03 18:25:18 -0700

    chore: 🔧 sort release config options lexicographically
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- release.config.ts
+++ release.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "@theholocron/semantic-release-config";
 
 export default defineConfig({
+	assets: ["CHANGELOG.md", "docs/package.json", "package.json"],
 	branches: ["main", { name: "alpha", prerelease: true }],
-	npm: { access: "public" },
 	exec: {
 		prepareCmd: "pnpm exec holocron npm bump-versions ${nextRelease.version}",
 		publishCmd: "pnpm --filter @theholocron/node-template-site publish --access public --no-git-checks",
 	},
-	assets: ["CHANGELOG.md", "package.json", "docs/package.json"],
+	npm: { access: "public" },
 });
